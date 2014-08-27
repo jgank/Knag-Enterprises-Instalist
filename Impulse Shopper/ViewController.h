8d13835f8cdf1203ce9759cfc01d1fe6f3dff237
Justin Knag <justin_knag@NY1MD1531N.local>
2014-08-27 18:19:42 -0400

    Initial Commit

---
+++ Impulse Shopper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Impulse Shopper
+//
+//  Created by Justin Knag on 8/27/14.
+//  Copyright (c) 2014 Justin Knag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
