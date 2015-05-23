8c8a5a797ed36ee51f8ea2729cb31e7c2070130b
Justin Knag <justinknag@gmail.com>
2015-05-22 21:23:27 -0400

    fixed syntax error, working on Watch App

---
+++ Instalist WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Instalist WatchKit Extension
+//
+//  Created by Justin Knag on 5/22/15.
+//  Copyright (c) 2015 Justin Knag. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
---
+++ Instalist WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Instalist WatchKit Extension
+//
+//  Created by Justin Knag on 5/22/15.
+//  Copyright (c) 2015 Justin Knag. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
